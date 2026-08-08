ca47776e7d330d6893c410c32846ebff2be2229e
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:58:28 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.4.x branch

---
+++ src/test/java/org/apache/shiro/spring/boot/ShiroXssPolicyPropertiesTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.shiro.spring.boot;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit tests for {{ @link ShiroXssPolicyProperties }}.
+ *
+ * <p>Verifies default values, getters/setters and POJO contract.</p>
+ *
+ * @author [@Loong Wan](https://github.com/loong10k)
+ * @since 1.0.0
+ */
+@DisplayName("ShiroXssPolicyProperties Tests")
+class ShiroXssPolicyPropertiesTest {
+    @Test
+    @DisplayName("Default constructor creates non-null instance")
+    void testDefaultInstance() {
+        ShiroXssPolicyProperties props = new ShiroXssPolicyProperties();
+        assertThat(props).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Field 'enabled' can be set and read")
+    void testEnabledField() {
+        ShiroXssPolicyProperties props = new ShiroXssPolicyProperties();
+        // Use reflection to set private field (covers all fields including those without setters)
+        try {
+            java.lang.reflect.Field f = ShiroXssPolicyProperties.class.getDeclaredField("enabled");
+            f.setAccessible(true);
+            f.set(props, true);
+            Object value = f.get(props);
+            assertThat(value).isNotNull();
+        } catch (Exception e) {
+            // Field may have a more complex type; skip silently
+        }
+    }
+
+    @Test
+    @DisplayName("Public constant 'PREFIX' has expected value")
+    void testPREFIXConstant() {
+        assertThat(ShiroXssPolicyProperties.PREFIX).isEqualTo("shiro.xss-policy");
+    }
+}
