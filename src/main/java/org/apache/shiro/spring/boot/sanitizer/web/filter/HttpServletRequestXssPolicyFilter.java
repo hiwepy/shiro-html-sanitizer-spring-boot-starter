59f950e923a2931a096dfc9a6a7aff94266f5846
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:57:52 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 2.7.x branch

--- src/main/java/org/apache/shiro/spring/boot/sanitizer/web/filter/HttpServletRequestXssPolicyFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/sanitizer/web/filter/HttpServletRequestXssPolicyFilter.java
@@ -2,12 +2,12 @@ package org.apache.shiro.spring.boot.sanitizer.web.filter;
 
 import java.io.IOException;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.spring.boot.sanitizer.web.servlet.http.HttpServletXssPolicyRequestWrapper;
 import org.apache.shiro.web.filter.AccessControlFilter;
@@ -16,7 +16,7 @@ import org.owasp.html.PolicyFactory;
 
 /**
  * XSS(Cross Site Scripting)，即跨站脚本攻击请求过滤
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class HttpServletRequestXssPolicyFilter extends AccessControlFilter {
 	
--- src/main/java/org/apache/shiro/spring/boot/sanitizer/web/servlet/http/HttpServletXssPolicyRequestWrapper.java
+++ src/main/java/org/apache/shiro/spring/boot/sanitizer/web/servlet/http/HttpServletXssPolicyRequestWrapper.java
@@ -5,9 +5,9 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
 
 import org.apache.shiro.spring.boot.sanitizer.utils.XssScanUtils;
 import org.owasp.html.PolicyFactory;
@@ -16,7 +16,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  * XSS(Cross Site Scripting)，即跨站脚本攻击请求过滤;
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class HttpServletXssPolicyRequestWrapper extends HttpServletRequestWrapper {
 
