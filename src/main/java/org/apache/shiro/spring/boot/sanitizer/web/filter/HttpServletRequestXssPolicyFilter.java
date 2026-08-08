0910cecaaaf8d64f014e22d7b2acbf8ecc7829e9
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 20:25:36 +0800

    docs: correct @author tag to [@Loong Wan](https://github.com/loong10k)
    - Replace incorrect 'wandl' / 'hiwepy' / 'easy-4-java contributors' attributions
      with the canonical author reference [@Loong Wan](https://github.com/loong10k)
    - Apply to both generated unit tests and existing source files
    - Consistent with project maintenance ownership

--- src/main/java/org/apache/shiro/spring/boot/sanitizer/web/filter/HttpServletRequestXssPolicyFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/sanitizer/web/filter/HttpServletRequestXssPolicyFilter.java
@@ -16,7 +16,7 @@ import org.owasp.html.PolicyFactory;
 
 /**
  * XSS(Cross Site Scripting)，即跨站脚本攻击请求过滤
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class HttpServletRequestXssPolicyFilter extends AccessControlFilter {
 	
--- src/test/java/org/apache/shiro/spring/boot/sanitizer/web/servlet/http/PolicyEnumerationTest.java
+++ src/test/java/org/apache/shiro/spring/boot/sanitizer/web/servlet/http/PolicyEnumerationTest.java
@@ -23,7 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Unit tests for {{ @link PolicyEnumeration }}.
  *
- * @author wandl
+ * @author [@Loong Wan](https://github.com/loong10k)
  * @since 1.0.0
  */
 @DisplayName("PolicyEnumeration Tests")
