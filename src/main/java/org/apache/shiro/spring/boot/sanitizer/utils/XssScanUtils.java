d8ca39c501da8eef01cdc9927bcb5b649bd0ee6e
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 12:30:34 +0800

    docs: add comprehensive English Javadoc

--- src/main/java/org/apache/shiro/spring/boot/sanitizer/utils/XssScanUtils.java
+++ src/main/java/org/apache/shiro/spring/boot/sanitizer/utils/XssScanUtils.java
@@ -17,6 +17,7 @@ package org.apache.shiro.spring.boot.sanitizer.utils;
 
 import org.apache.commons.lang3.ArrayUtils;
 
+/**\n * Utility class for XSS scanning and HTML sanitization.\n *\n * @author [@Loong Wan](https://github.com/loong10k)\n * @since 1.0.0\n */
 public class XssScanUtils {
 	
 	public static boolean isXssHeader(String[] policyHeaders, String name) {
